49063d0bdce0bebc59d2bee7a3a88ae2f011072a
Suhaib Chowdhury <ogs1492@gmail.com>
2024-05-07 23:43:19 -0700

    Explore pt 3

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Lab 5 - Starter
 Suhaib Chowdhury
+
+1. No. Testing "Message" would require testing an interaction between multiple components, so unit testing is not suitable.
+
+2. Yes. This is a simple test of checking if a condition holds.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,55 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+
+test('Phone number true 1', () => {
+  expect(isPhoneNumber("559-789-3424")).toBe(true);
+});
+
+test('Phone number true 2', () => {
+  expect(isPhoneNumber("423-423-6463")).toBe(true);
+});
+
+test('Phone number fasle 1', () => {
+  expect(isPhoneNumber("1111")).toBe(false);
+});
+
+test('Phone number fasle 2', () => {
+  expect(isPhoneNumber("")).toBe(false);
+});
+
+test('Email true 1', () => {
+  expect(isEmail("ashu@ui.com")).toBe(true);
+});
+
+test('Email true 2', () => {
+  expect(isEmail("adssd@uir.com")).toBe(true);
+});
+
+test('Email false 1', () => {
+  expect(isEmail("ashu@uicom")).toBe(false);
+});
+
+test('Email false 2', () => {
+  expect(isEmail("ashuui")).toBe(false);
+});
+
+test('Strong true 1', () => {
+  expect(isStrongPassword("rsftw")).toBe(true);
+});
+
+test('Strong true 2', () => {
+  expect(isStrongPassword("twxggtr")).toBe(true);
+});
+
+test('Strong false 1', () => {
+  expect(isStrongPassword("4444")).toBe(false);
+});
+
+test('Strong false 2', () => {
+  expect(isStrongPassword("yjeei&")).toBe(false);
+});
+
+test('Date true 1', () => {
+  expect(isStrongPassword("4444")).toBe(false);
+});
